3e2018b0cbbae625655a0a0b53cd32f608f46e9e
TeresaUll <tbonetcosta@gmail.com>
2022-08-24 21:38:41 +0200

    compila

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+// Components
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { AuthGuard } from './auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+ 
+  {
+    path: 'signin',
+    component: SigninComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
