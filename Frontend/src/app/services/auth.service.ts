bf8d98d041b73deea0d2e68c8e45d6cd5efd8003
TeresaUll <tbonetcosta@gmail.com>
2022-08-26 19:32:44 +0200

    funciona

--- Frontend/src/app/services/auth.service.ts
+++ Frontend/src/app/services/auth.service.ts
@@ -33,8 +33,8 @@ export class AuthService {
   addPartido(partido: any){
     return this.http.post(this.URL + '/createpartido', partido);
   }
-  partidas() {
-    return this.http.get(this.URL + '/partidas');
+  getPartidas() {
+    return this.http.get<any>(this.URL + '/partidas');
   }
-
+  
 }
