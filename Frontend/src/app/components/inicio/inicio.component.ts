c9f2b1a73808dc9f52e3b10d96167d28fe7a4077
Marta163gg <mggalvan@minsait.com>
2022-08-26 13:24:37 +0200

    changes bootstrap

--- Frontend/src/app/components/inicio/inicio.component.ts
+++ Frontend/src/app/components/inicio/inicio.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'inicio',
-  templateUrl: './inicio.component.html'
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit{
-  title = ' Inicio ';
+  title = ' Bienvenido a nuestro club de tennis';
+  lugar = 'en la Laguna!'
 
   ngOnInit(){
     console.log('incio.component cargado!!')
